59e9f4e1aacdc87a18612add26d2e3c4af05aba5
zhangysa <zhangysa@jiedaibao.com>
2018-10-26 17:39:30 +0800

    add matchzoo to make match

---
+++ MatchZoo/examples/toy_example/generate_ranking_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# generate matchzoo data for ranking
+python test_preparation_for_ranking.py
+
+# 2. map word embedding
+python gen_w2v.py ../../data/toy_example/ranking/glove.6B.50d.txt ../../data/toy_example/ranking/word_dict.txt ../../data/toy_example/ranking/embed_glove_d50
+python norm_embed.py  ../../data/toy_example/ranking/embed_glove_d50 ../../data/toy_example/ranking/embed_glove_d50_norm
+
+# 3. run to generate histogram for DRMM
+python test_histogram_generator.py  'ranking'
+
+# 4. run to generate tri-grams for DSSM or CDSSM
+python test_triletter_preprocess.py 'ranking'
+
+# 5. run to generate binsum for aNMM
+python test_binsum_generator.py 'ranking'
+
--- javadoc-parser/delet1.sql
+++
@@ -1,8 +0,0 @@
-
-use Parser;
-Truncate Table interface;
-Truncate Table package;
-Truncate Table class;
-Truncate Table enum;
-Truncate Table excption;
-Truncate Table error;
